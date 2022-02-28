70b6fbf9decf67f609a07f9ca1c41d6c22a015a6
Tefylbr <93145301+Tefylbr@users.noreply.github.com>
2022-02-27 22:59:33 -0600

    Add files via upload

--- Tarea4_lab/Tarea4_lab/Form1.Designer.vb
+++ Tarea4_lab/Tarea4_lab/Form1.Designer.vb
@@ -27,13 +27,15 @@ Partial Class Form1
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Hey Haters", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label1.Location = New System.Drawing.Point(147, 120)
+        Me.Label1.Location = New System.Drawing.Point(147, 50)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(530, 38)
         Me.Label1.TabIndex = 0
@@ -41,7 +43,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(293, 259)
+        Me.Button1.Location = New System.Drawing.Point(264, 169)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(94, 29)
         Me.Button1.TabIndex = 1
@@ -50,14 +52,14 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(293, 196)
+        Me.TextBox1.Location = New System.Drawing.Point(293, 113)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(211, 27)
         Me.TextBox1.TabIndex = 2
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(410, 259)
+        Me.Button2.Location = New System.Drawing.Point(410, 169)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(94, 29)
         Me.Button2.TabIndex = 3
@@ -68,17 +70,37 @@ Partial Class Form1
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Poor Richard", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label2.Location = New System.Drawing.Point(333, 326)
+        Me.Label2.Location = New System.Drawing.Point(328, 223)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(133, 35)
         Me.Label2.TabIndex = 4
         Me.Label2.Text = "Resultado "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Poor Richard", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(305, 340)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(182, 35)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Multiplos de 4 "
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(32, 290)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(698, 27)
+        Me.TextBox2.TabIndex = 7
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.TextBox1)
@@ -96,4 +118,6 @@ Partial Class Form1
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Button2 As Button
     Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox2 As TextBox
 End Class
--- Tarea4_lab/Tarea4_lab/Form1.vb
+++ Tarea4_lab/Tarea4_lab/Form1.vb
@@ -1,9 +1,11 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim i, j, n As Integer
-        Dim Esperar As Char
+        Dim i, j, n, z, k As Integer
         n = TextBox1.Text
-
+        Dim indice = 0
+        Dim resultado1 As String = ""
+        Dim cantidad As String = 0
+        Dim ciclo As String
         Try
             j = 1
             'formula para calculo n! = 1 x 2 x 3 x 4 x 5 x ... x (n-1) x n
@@ -11,14 +13,35 @@
                 j = j * i
             Next
             Label2.Text = j
+            For z = 0 To j Step 1
+                ciclo = ciclo + z.ToString + ", "
+            Next
+            TextBox2.Text = ciclo
+            While (indice <= j)
+                If (indice Mod 4 = 0) Then
+                    cantidad = cantidad + 1
+                End If
+                indice = indice + 1
+            End While
+            Label4.Text = cantidad
+
         Catch ex As Exception
             MsgBox(ex.Message)
         End Try
     End Sub
-
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         TextBox1.Text = ""
         Label2.Text = "Resultado"
+        TextBox2.Text = ""
+        Label4.Text = "Multiplos de 4"
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
 
     End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
 End Class
